64b4963278f6dcb8ebf279c25b0420672690371e
do3642 <61033883+do3642@users.noreply.github.com>
2024-10-10 22:00:01 +0900

    domain폴더에 port 테이블 준비완료 (외래키는 확인해봐야함)

---
+++ src/main/java/com/example/recipe/domain/Post.java
@@ -0,0 +1,51 @@
+package com.example.recipe.domain;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Post {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable = false, length = 100)
+	private String title;
+	
+	@Lob //varchar은 길이제한이 있어 대용량 내용을 넣을 수 있는 데이터타입 선언
+	@Column(nullable = false)
+	private String content;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+	private int cnt;
+	
+	// 엔티티는 user의 id만 가져오기 번거로우니
+	// 객체 형태로 가져온다, 관계는 설정해야함 1:N상태이니 ManyToOne
+	@ManyToOne(fetch = FetchType.EAGER)
+	// EAGER ->게시글 정보 로드 될때 동시에 유저정보를 가져옴, LAZY->사용할 때 가져옴
+	
+	@JoinColumn(name = "clientid") // 외래키 설정
+	private Client client;
+	
+}
--- src/main/webapp/WEB-INF/recipe/index.jsp
+++ src/main/webapp/WEB-INF/recipe/index.jsp
@@ -4,13 +4,21 @@
 <%@ include file="./layout/header.jsp" %>
 <title>요리연구소</title>
 
-
+<link rel="stylesheet" href="/css/post.css">
 <%@ include file="./layout/nav.jsp" %>
 
 
 	<section>
-	<h1>레시피 index 페이지</h1>
-	
+		<article class="items-board">
+			<div>게시판1</div>
+			<div>게시판2</div>
+			<div>게시판3</div>
+			<div>게시판4</div>
+			<div>게시판5</div>
+			<div>게시판6</div>
+			<div>게시판7</div>
+			<div>게시판8</div>
+		</article>
 	</section>
 	
 
