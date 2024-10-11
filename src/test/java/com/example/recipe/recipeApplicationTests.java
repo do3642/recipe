d1de0190801c5943d5d61cbfdfbcbf47a3b0636a
yc <do36421@naver.com>
2024-10-11 10:56:21 +0900

    임시 게시물을 넣는 테스트파일 작성완료

--- src/test/java/com/example/recipe/recipeApplicationTests.java
+++ src/test/java/com/example/recipe/recipeApplicationTests.java
@@ -1,13 +1,38 @@
 package com.example.recipe;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.recipe.domain.Client;
+import com.example.recipe.domain.Post;
+import com.example.recipe.repository.PostRepository;
+import com.example.recipe.repository.UserRepository;
 
 @SpringBootTest
 class recipeApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Test
 	void contextLoads() {
+		
+		Client client = userRepository.findById(5).get();
+		
+		for(int i=30; i<=60; i++) {
+			
+		
+		Post post = new Post();
+		post.setTitle("임시 게시물 제목"+i);
+		post.setContent("임시 게시물 내용"+i);
+		post.setClient(client);
+		post.setThumbnail("임시이미지주소"+i);
+		
+		postRepository.save(post);
+		}
 	}
 
 }
